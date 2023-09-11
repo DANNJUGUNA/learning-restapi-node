2ed13595a9aa89e8cd043ea43858a1f5836f8356
DANNJUGUNA <ndan30071@gmail.com>
2023-09-11 11:54:28 +0300

    created a route and tested it and its woking

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import bodyParser from "body-parser";
 const app=express();
 const PORT=5000;
 
+
 app.use(bodyParser.json()); //this tells the app that we will use data as a json
 
-app.listen(PORT, ()=>console.log(`Server running on port : http://localhost/${PORT}`))
+app.get('/',(req, res) => {
+    console.log("TEST")
+
+    res.send("Hello From home page");
+});
+app.listen(PORT, ()=>console.log(`Server running on port : http://localhost:${PORT}`))
