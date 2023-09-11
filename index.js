37b42729163d460e8186566df402686822463b0f
DANNJUGUNA <ndan30071@gmail.com>
2023-09-11 11:41:15 +0300

    updated what the app should listen whin the sever is run

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import bodyParser from "body-parser";
 
 const app=express();
 const PORT=5000;
+
+app.use(bodyParser.json()); //this tells the app that we will use data as a json
+
+app.listen(PORT, ()=>console.log(`Server running on port : http://localhost/${PORT}`))
