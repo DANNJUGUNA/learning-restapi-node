b741cc329279f80781314f2d6d38778366d78b1c
DANNJUGUNA <ndan30071@gmail.com>
2023-09-12 10:59:17 +0300

    updated code

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 import express from 'express';
 
 const router=express.Router();
+const users=[
+    {
+        "firstName":"John",
+        "lastName":"Doe",
+        "age":25
+    }
+]
 //all routes are starting with /users
 router.get('/', (req, res) => {
      res.send('HELLO')
