37ba2e3a2e5eb0f5263a45a733ba635252e3fa80
DANNJUGUNA <ndan30071@gmail.com>
2023-09-13 11:35:08 +0300

    implemented delete a user

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,17 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
 const router=express.Router();
-const users=[
+let users=[
     {
         "firstName":"John",
         "lastName":"Doe",
         "age":25,
         "id":"1"
+    },{
+        "firstName":"John",
+        "lastName":"Doe",
+        "age":25,
+        "id":"2"
     }
 ]
 //all routes are starting with /users
@@ -24,6 +29,11 @@ router.get('/:id',(req, res) => {
     const {id}=req.params;
    const foundUser= users.find((user)=>user.id===id)
     res.send(foundUser)
-})
+});
+router.delete('/:id', (req, res) => {
+    const {id}=req.params;
+    users=users.filter((user)=>user.id !==id)
+    res.send(`user with user name ${id} has been deleted`)
+});
 
 export default router;
